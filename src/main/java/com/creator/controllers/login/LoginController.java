94d2ffa6b1f7f5bfd7e646bb07683f55b04b26f8
pravindap <pravinda.perera@trycake.com>
2017-07-23 19:35:56 +0530

    routing completed for the home page

--- src/main/java/com/creator/controllers/login/LoginController.java
+++ src/main/java/com/creator/controllers/login/LoginController.java
@@ -18,7 +18,7 @@ public class LoginController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLoginPage() {
-		return "home";
+		return "login";
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
